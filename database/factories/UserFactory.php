a45c39190d9b25c15265d1183d6e54b9afb27432
alaa140101 <alaa140011@gmail.com>
2021-10-08 07:47:24 +0300

    fake posts and users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->username,
+            'bio' => $this->faker->text(100),
+            'url' => $this->faker->url,
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
