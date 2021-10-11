ffa7d2aba0bdd3dafb5d70c54aca9b15534e6301
alaa140101 <alaa140011@gmail.com>
2021-10-11 14:09:27 +0300

    prepare likes option

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
@@ -21,6 +22,31 @@ class Post extends Model
 
     public function comments() {
         return $this->hasMany(Comment::class)->orderBy('created_at', 'DESC');
+    }
+
+    public function likedByUsers() {
+        return $this->belongsToMany(User::class, 'likes');
+    }
+
+    public function like(User $user) {
+        return $this->likedByUsers()->save($user);
+    }
+
+    public function dislike(User $user) {
+        return $this->likedByUsers()->detach($user);
+    }
+
+    public function likedByUser(User $user) {
+        return (bool)DB::table('likes')->where('user_id', $user->id)->where('post_id', $this->id)->count();
+    }
+
+    public function likeSystem(User $user) {
+        
+        if($this->likedByUser($user)) {
+            return $this->dislike($user);
+        }else {
+            return $this->like($user);
+        }
     }
 
 }
