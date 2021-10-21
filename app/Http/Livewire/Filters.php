876dbaaf41b3f0115cc588882a6d9a6496fec257
alaa140101 <alaa140011@gmail.com>
2021-10-21 13:56:33 +0300

    error upload image

--- app/Http/Livewire/Filters.php
+++ app/Http/Livewire/Filters.php
@@ -10,32 +10,88 @@ class Filters extends Component
     public $post_caption;
     public $image_path;
 
+    public $filter1 = "./storage/uploads/1.jpeg";
+    public $filter2 = "./storage/uploads/2.jpeg";
+    public $filter3 = "./storage/uploads/3.jpeg";
+    public $filter4 = "./storage/uploads/4.jpeg";
+    public $filter5 = "./storage/uploads/5.jpeg";
+    public $filter6 = "./storage/uploads/6.jpeg";
+    public $filter7 = "./storage/uploads/7.jpeg";
+    public $filter8 = "./storage/uploads/8.jpeg";
+    public $filter9 = "./storage/uploads/9.jpeg";
+    public $filter10 = "./storage/uploads/10.jpeg";
+    public $filter11 = "./storage/uploads/11.jpeg";
+    public $filter12 = "./storage/uploads/12.jpeg";
+    public $filter13 = "./storage/uploads/13.jpeg";
+
     public function mount($post_caption, $image_path)
     {
         $this->post_caption = $post_caption;
         $this->image_path = $image_path;
 
-        $img = imagecreatefromstring(file_get_contents("storage/".$this->image_path));
-        imagefilter($img, IMG_FILTER_NEGATE);
-        imagejpeg($img, "./storage/uploads/1.jpeg", 100);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_NEGATE, $this->filter1, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_GRAYSCALE, $this->filter2, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_BRIGHTNESS, $this->filter3, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_CONTRAST, $this->filter4, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_COLORIZE, $this->filter5, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_EDGEDETECT, $this->filter6, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_GAUSSIAN_BLUR, $this->filter7, null, null, null, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_SELECTIVE_BLUR, $this->filter8, 100, 50, 0, null, 0);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_EMBOSS, $this->filter9, 20, null, null, null, 1);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_MEAN_REMOVAL, $this->filter10, 80, null, null, null, 1);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_SMOOTH, $this->filter11, 20, null, null, null, 1);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_PIXELATE, $this->filter12, 50, null, null, null, 2);
+        $this->correctImageOrientation($this->image_path, IMG_FILTER_SCATTER, $this->filter13, 80, null, null, null, 1);
     }
 
     public function applyFilter($num) {
         switch ($num) {
             case 0:
-                auth()->user()->posts()->create([
-                    'post_caption' => $this->post_caption,
-                    'image_path' => $this->image_path,
-                ]);
-                break;
-                case 1:
-                    rename("./storage/uploads/1.jpeg", "storage/".$this->image_path);
-                    auth()->user()->posts()->create([
-                        'post_caption' => $this->post_caption,
-                        'image_path' => $this->image_path,
-                    ]);
-                    break;
+            break;
+            case 1:
+                rename($this->filter1, "storage/".$this->image_path);
+            break;
+            case 2:
+                rename($this->filter2, "storage/".$this->image_path);
+            break;
+            case 3:
+                rename($this->filter3, "storage/".$this->image_path);
+            break;
+            case 4:
+                rename($this->filter4, "storage/".$this->image_path);
+            break;
+            case 5:
+                rename($this->filter5, "storage/".$this->image_path);
+            break;
+            case 6:
+                rename($this->filter6, "storage/".$this->image_path);
+            break;
+            case 7:
+                rename($this->filter7, "storage/".$this->image_path);
+            break;
+            case 8:
+                rename($this->filter8, "storage/".$this->image_path);
+            break;
+            case 9:
+                rename($this->filter9, "storage/".$this->image_path);
+            break;
+            case 10:
+                rename($this->filter10, "storage/".$this->image_path);
+            break;
+            case 11:
+                rename($this->filter11, "storage/".$this->image_path);
+            break;
+            case 12:
+                rename($this->filter12, "storage/".$this->image_path);
+            break;
+            case 13:
+                rename($this->filter13, "storage/".$this->image_path);
+            break;               
         }
+        auth()->user()->posts()->create([
+            'post_caption' => $this->post_caption,
+            'image_path' => $this->image_path,
+        ]);
         return redirect()->route('user_profile', ['username' => auth()->user()->username]);
     }
 
@@ -43,4 +99,107 @@ class Filters extends Component
     {
         return view('livewire.filters');
     }
+
+    public function correctImageOrientation($imagePath, $effect, $newImagePath, $arg1, $arg2, $arg3, $arg4, $argnum) 
+    {
+        $img = imagecreatefromstring(file_get_contents("storage/".$imagePath));
+        if(@exif_read_data("storage/".$imagePath)){
+            $exif = @exif_read_data("storage/".$imagePath);
+            if($exif && isset($exif['Orientation'])){
+                $orientation = $exif['Orientation'];
+                if($orientation != 1){
+                    $deg = 0;
+                    // orientation
+                    switch ($orientation) {
+                        case 3: 
+                            $deg = 180;
+                            $img = imagerotate($img, $deg, 0);
+                            break;
+                        case 6: 
+                            $deg = 270;
+                            $img = imagerotate($img, $deg, 0);
+                            break;
+                        case 8:
+                            $deg = 90;
+                            $img = imagerotate($img, $deg, 0);
+                            break;
+                        default: 
+                            $img = imagerotate($img, $deg, 0);
+                            break;
+                    }
+                    
+                    //filter
+                    switch ($argnum){
+                        case '0': 
+                            imagefilter($img, $effect);
+                            break;
+                        case '1': 
+                            imagefilter($img, $effect, $arg1);
+                            break;
+                        case '2': 
+                            imagefilter($img, $effect, $arg1, $arg2);
+                            break;
+                        case '3': 
+                            imagefilter($img, $effect, $arg1, $arg2, $arg3);
+                            break;
+                        case '4': 
+                            imagefilter($img, $effect, $arg1, $arg2, $arg3, $arg4);
+                            break;
+                    }
+
+                    imagejpeg($img, $newImagePath, 100);
+                    imagedestroy($img);
+                }
+            }
+            else{
+                //filter
+                switch ($argnum){
+                   case '0': 
+                       imagefilter($img, $effect);
+                       break;
+                   case '1': 
+                       imagefilter($img, $effect, $arg1);
+                       break;
+                   case '2': 
+                       imagefilter($img, $effect, $arg1, $arg2);
+                       break;
+                   case '3': 
+                       imagefilter($img, $effect, $arg1, $arg2, $arg3);
+                       break;
+                   case '4': 
+                       imagefilter($img, $effect, $arg1, $arg2, $arg3, $arg4);
+                       break;
+               }
+   
+               imagejpeg($img, $newImagePath, 100);
+               imagedestroy($img);
+               
+               }        
+            }
+            else{
+             //filter
+             switch ($argnum){
+                case '0': 
+                    imagefilter($img, $effect);
+                    break;
+                case '1': 
+                    imagefilter($img, $effect, $arg1);
+                    break;
+                case '2': 
+                    imagefilter($img, $effect, $arg1, $arg2);
+                    break;
+                case '3': 
+                    imagefilter($img, $effect, $arg1, $arg2, $arg3);
+                    break;
+                case '4': 
+                    imagefilter($img, $effect, $arg1, $arg2, $arg3, $arg4);
+                    break;
+            }
+
+            imagejpeg($img, $newImagePath, 100);
+            imagedestroy($img);
+            
+            }
+    }
+
 }
--- resources/views/livewire/filters.blade.php
+++ resources/views/livewire/filters.blade.php
@@ -1,4 +1,61 @@
-<div>
-    <img src="/storage/{{$image_path}}" alt="" class="w-40 h-40" wire:model="filters" wire:click="applyFilter(0)">
-    <img src="/storage/uploads/1.jpeg" alt="" class="w-40 h-40" wire:model="filters" wire:click="applyFilter(1)">
+<div class="max-w-4xl my-0 mx-auto">
+    <div class="grid grid-cols-3 gap-4 mx-0 mt-0 mb-0">
+        <div>
+            <p class="text-center">{{__('original')}}</p>
+            <img src="/storage/{{$image_path}}" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(0)">
+        </div>
+        <div>
+            <p class="text-center">{{__('Negative')}}</p>
+            <img src="/storage/uploads/1.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(1)">
+        </div>
+        <div>
+            <p class="text-center">{{__('Grayscale')}}</p>
+            <img src="/storage/uploads/2.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(2)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter3')}}</p>
+            <img src="/storage/uploads/3.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(3)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter4')}}</p>
+            <img src="/storage/uploads/4.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(4)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter5')}}</p>
+            <img src="/storage/uploads/5.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(5)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter6')}}</p>
+            <img src="/storage/uploads/6.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(6)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter7')}}</p>
+            <img src="/storage/uploads/7.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(7)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter8')}}</p>
+            <img src="/storage/uploads/8.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(8)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter9')}}</p>
+            <img src="/storage/uploads/9.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(9)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter10')}}</p>
+            <img src="/storage/uploads/10.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(10)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter11')}}</p>
+            <img src="/storage/uploads/11.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(11)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter12')}}</p>
+            <img src="/storage/uploads/12.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(12)">
+        </div>
+        <div>
+            <p class="text-center">{{__('filter13')}}</p>
+            <img src="/storage/uploads/13.jpeg" alt="" class="w-64 h-64 cursor-pointer" wire:model="filters" wire:click="applyFilter(13)">
+        </div>
+        
+    </div>
 </div>
